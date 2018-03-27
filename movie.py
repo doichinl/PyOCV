dd55df4a60145ddcbcb653f0d99bbac67a4d3787
Doychin Lazarov <Doychin_Lazarov@epam.com>
2018-03-27 14:28:15 +0300

    Use Class

--- movie.py
+++ movie.py
@@ -3,73 +3,207 @@ import cv2
 import copy
 import time
 
-# cap = cv2.VideoCapture('http://api.new.livestream.com/accounts/7970204/events/2926299/live.m3u8')
-cap = cv2.VideoCapture('video.mp4')
+class Topview:
+    width=640
+    height=480
 
-if cap.isOpened():
-    ret, frame = cap.read()
-    if ret==True:
-        mask = np.zeros(frame.shape, dtype = "uint8")
-        # Draw a white, filled rectangle on the mask image
-        oh, ow = frame.shape[:2]
-        pts = np.array([[0,0],[ow,0],[int(ow*3/4),oh],[int(ow/4),oh]], np.int32)
-        pts = pts.reshape((-1,1,2))
-        cv2.fillPoly(mask, [pts], (255, 255, 255))
+    def __init__(self, front, back, left, right):
+        self.front = front
+        self.back = back
+        self.left = left
+        self.right = right
 
+    def capture(self):
+        self.__init_capture()
+        self.__make_mask()
 
-        while(cap.isOpened()):
-            ret, frame = cap.read()
-            if ret==True:
-                start = time.time()
-                # FIXME this is for visual purpose only
-                frame = cv2.bitwise_not(frame)
-                # Apply the mask
-                maskedImg = cv2.bitwise_and(frame, mask)
-                # Resize image to half of it
-                maskedImg = cv2.resize(maskedImg, (0,0), fx=0.5, fy=0.5)
-                h, w = maskedImg.shape[:2]
+        while(True):
+            start = time.time()
 
-                # Change perspective
-                pts1 = np.float32([[0,0],[w,0],[0,h],[w,h]])
-                pts2 = np.float32([[0,0],[w,0],[0,int(w/4)],[w,int(w/4)]])
+            res = self.__get_screen()
 
-                M = cv2.getPerspectiveTransform(pts1,pts2)
-                maskedImg = cv2.warpPerspective(maskedImg,M,(w,int(w/4)))
+            front = self.__capture_front()
+            back = self.__capture_back()
+            left = self.__capture_left()
+            right = self.__capture_right()
 
-                h, w = maskedImg.shape[:2]
-                
-                #create empty matrix
-                visTB = np.zeros((h*2 + int(w/2), h*2 + int(w/2), 3), np.uint8)
-                visLR = copy.copy(visTB)
+            if isinstance(front, np.ndarray):
+                res = cv2.bitwise_or(res, front)
+            if isinstance(back, np.ndarray):
+                res = cv2.bitwise_or(res, back)
+            if isinstance(left, np.ndarray):
+                res = cv2.bitwise_or(res, left)
+            if isinstance(right, np.ndarray):
+                res = cv2.bitwise_or(res, right)
 
-                #combine 4 images
-                # TOP
-                visTB[:h, h-int(w/4):h-int(w/4)+w, :3] = maskedImg
-                # BOTTOM
-                visTB[h+int(w/2):h*2 + int(w/2), h-int(w/4):h-int(w/4)+w, :3] = cv2.flip(maskedImg, -1)
-
-                # LEFT
-                M = cv2.getRotationMatrix2D((w/2, w/2), 90, 1)
-                left = cv2.warpAffine(maskedImg, M, (w, w))
-                left = left[0:w, 0:h]
-                visLR[h-int(w/4):w+h-int(w/4), :h, :3] = left
-                # RIGHT
-                M = cv2.getRotationMatrix2D((w/2, w/2), -90, 1)
-                right = cv2.warpAffine(maskedImg, M, (w, w))
-                right = right[0:w, w-h:w]
-                visLR[h-int(w/4):w+h-int(w/4), h + int(w/2):h*2 + int(w/2), :3] = right
-                
-                cv2.imshow('frame', cv2.bitwise_or(visLR, visTB))
-                # print('END', time.time() - start)
-                
-                # ~ 24 frames per second
-                time.sleep(0.04 - (time.time() - start))
-                
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
-            else:
+            # Show combined image
+            cv2.imshow('frame', res)
+            
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            
+            if (
+                not(isinstance(front, np.ndarray)) and
+                not(isinstance(back, np.ndarray)) and
+                not(isinstance(left, np.ndarray)) and
+                not(isinstance(right, np.ndarray))
+            ):
                 break
 
-# Release everything if job is finished
-cap.release()
-cv2.destroyAllWindows()
+            # ~ 24 frames per second
+            if time.time() - start < 0.04:
+                time.sleep(0.04 - (time.time() - start))
+        
+        # Release everything if job is finished
+        self.frontCapture.release()
+        self.backCapture.release()
+        self.leftCapture.release()
+        self.rightCapture.release()
+        cv2.destroyAllWindows()
+
+    def __init_capture(self):
+        self.frontCapture = cv2.VideoCapture(self.front)
+        self.backCapture = cv2.VideoCapture(self.back)
+        self.leftCapture = cv2.VideoCapture(self.left)
+        self.rightCapture = cv2.VideoCapture(self.right)
+
+    def __get_screen(self):
+        return np.zeros(
+                (
+                    self.height*2 + int(self.width/2),
+                    self.height*2 + int(self.width/2),
+                    3
+                ),
+                np.uint8
+            )
+
+    def __make_mask(self):
+        self.mask = np.zeros((self.height, self.width, 3), np.uint8)
+        # Draw a white, filled rectangle on the mask image
+        pts = np.array(
+            [
+                [0, 0],
+                [self.width, 0],
+                [int(self.width*3/4), self.height],
+                [int(self.width/4), self.height]
+            ],
+            np.int32
+        )
+        pts = pts.reshape((-1, 1, 2))
+        cv2.fillPoly(self.mask, [pts], (255, 255, 255))
+
+    def __apply_mask(self, frame):
+        # Resize image to fit self.width and self.height
+        frame = cv2.resize(frame, (self.width, self.height), fx=0, fy=0)
+
+        # Apply the mask
+        frame = cv2.bitwise_and(frame, self.mask)
+        return frame
+
+    def __change_perspective(self, frame):
+        # FIXME Do not change perspective for now, until real data is not provided
+        return frame
+        # Change perspective
+        pts1 = np.float32([[0, 0], [self.width, 0], [0, self.height], [self.width, self.height]])
+        pts2 = np.float32([[0, 0], [self.width, 0], [0, int(self.width/4)], [self.width, int(self.width/4)]])
+
+        M = cv2.getPerspectiveTransform(pts1, pts2)
+        frame = cv2.warpPerspective(frame, M, (self.width, self.height))
+        return frame
+
+    def __capture_front(self):
+        if not(self.frontCapture.isOpened):
+            return None
+
+        ret, frame = self.frontCapture.read()
+        if ret == True:
+            frame = self.__apply_mask(frame)
+            frame = self.__change_perspective(frame)
+
+            #create empty matrix
+            vis = self.__get_screen()
+
+            # TOP
+            vis[
+                :self.height,
+                self.height-int(self.width/4):self.height-int(self.width/4)+self.width,
+                :3
+            ] = frame
+            return vis
+        
+        return None
+
+    def __capture_back(self):
+        if not(self.frontCapture.isOpened):
+            return None
+
+        ret, frame = self.frontCapture.read()
+        if ret == True:
+            frame = self.__apply_mask(frame)
+            frame = self.__change_perspective(frame)
+
+            #create empty matrix
+            vis = self.__get_screen()
+
+            # BACK
+            vis[
+                self.height+int(self.width/2):self.height*2 + int(self.width/2),
+                self.height-int(self.width/4):self.height-int(self.width/4)+self.width,
+                :3
+            ] = cv2.flip(frame, -1)
+            return vis
+        
+        return None
+
+    def __capture_left(self):
+        if not(self.frontCapture.isOpened):
+            return None
+
+        ret, frame = self.frontCapture.read()
+        if ret == True:
+            frame = self.__apply_mask(frame)
+            frame = self.__change_perspective(frame)
+
+            #create empty matrix
+            vis = self.__get_screen()
+
+            # LEFT
+            M = cv2.getRotationMatrix2D((self.width/2, self.width/2), 90, 1)
+            frame = cv2.warpAffine(frame, M, (self.width, self.width))
+            frame = frame[0:self.width, 0:self.height]
+            vis[
+                self.height-int(self.width/4):self.width+self.height-int(self.width/4),
+                :self.height,
+                :3
+            ] = frame
+            return vis
+        
+        return None
+
+    def __capture_right(self):
+        if not(self.frontCapture.isOpened):
+            return None
+
+        ret, frame = self.frontCapture.read()
+        if ret == True:
+            frame = self.__apply_mask(frame)
+            frame = self.__change_perspective(frame)
+
+            #create empty matrix
+            vis = self.__get_screen()
+
+            # RIGHT
+            M = cv2.getRotationMatrix2D((self.width/2, self.width/2), -90, 1)
+            frame = cv2.warpAffine(frame, M, (self.width, self.width))
+            frame = frame[0:self.width, self.width-self.height:self.width]
+            vis[
+                self.height-int(self.width/4):self.width+self.height-int(self.width/4),
+                self.height + int(self.width/2):self.height*2 + int(self.width/2),
+                :3
+            ] = frame
+            return vis
+        
+        return None
+
+topview = Topview('video.mp4', 'video.mp4', 'video.mp4', 'video.mp4')
+topview.capture()
